5aaa9c9f7425809760bb1aafcb053c152e762055
Weirdoer <akshiththota30@gmail.com>
2025-05-15 18:30:37 +0530

    init commit

--- src/main/java/com/fts/e_commerce/service/EcommerceService.java
+++ src/main/java/com/fts/e_commerce/service/EcommerceService.java
@@ -5,6 +5,9 @@ import com.fts.e_commerce.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class EcommerceService {
@@ -15,14 +18,23 @@ public class EcommerceService {
 
         // we need validate user details
         /*
-        * validate all user fields are not empty
-        * validate password :
-        * password should be of more than 8 (length)
-        * password should contain least one special character , one capital alphabet,
-        * smaller case alphabet , one digit
-        *
+         * validate all user fields are not empty
+         * validate password :
+         * password should be of more than 8 (length)
+         * password should contain least one special character , one capital alphabet,
+         * smaller case alphabet , one digit
+         *
          */
 
         userRepository.save(userEntity);
+    }
+
+    public List<UserEntity> searchUserDetails(String searchString) {
+        return userRepository.searchUserDetails(searchString);
+    }
+
+    public UserEntity getUserDetailsById(Integer id) {
+        Optional<UserEntity> user =  userRepository.findById(Long.valueOf(id));
+        return user.get();
     }
 }
