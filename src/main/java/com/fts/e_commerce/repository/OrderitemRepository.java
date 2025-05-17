3cf1faefe23d21ee6340a619149919040c1650b6
sumana <sumanabondalapati@gmail.com>
2025-05-17 09:34:18 +0530

    repos

---
+++ src/main/java/com/fts/e_commerce/repository/OrderitemRepository.java
@@ -0,0 +1,9 @@
+package com.fts.e_commerce.repository;
+
+import com.fts.e_commerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface OrderitemRepository extends JpaRepository<Product,Long> {
+}
---
+++ src/main/java/com/fts/e_commerce/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.fts.e_commerce.repository;
+
+import com.fts.e_commerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    @Query(value=);
+}
